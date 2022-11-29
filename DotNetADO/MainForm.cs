36ac74bf72933a43061e986229e308d951b25b23
洛神赋 <hluo5206@gmail.com>
2022-11-29 18:05:40 +0800

    feat: 事务的隔离级别

--- DotNetADO/MainForm.cs
+++ DotNetADO/MainForm.cs
@@ -22,15 +22,25 @@ namespace DotNetADO
 		// 隔离级别越高，事务的独立性越高，数据库的并发性越低。
 		private void MainFormLoad(object sender, System.EventArgs e)
 		{
-			new Thread(() =>
-			{
-				TestIsolationLevel1();
-			}).Start();
+			//new Thread(() =>
+			//{
+			//	TestIsolationLevel1();
+			//}).Start();
 
-			new Thread(() =>
-			{
-				TestIsolationLevel2();
-			}).Start();
+			//new Thread(() =>
+			//{
+			//	TestIsolationLevel2();
+			//}).Start();
+
+			//new Thread(() =>
+			//{
+			//	TestIsolationLevel3();
+			//	TestIsolationLevel3();
+			//}).Start();
+
+			// TestIsolationLevel4();
+			// TestIsolationLevel3();
+			// TestIsolationLevel3();
 		}
 
 		public void TestIsolationLevel1()
@@ -89,5 +99,58 @@ namespace DotNetADO
 			}
 		}
 
+		public void TestIsolationLevel3()
+		{
+			string source = "server=10.5.67.45;database=Northwind;uid=tomcat;pwd=LuoHao123;";
+			SqlConnection connection = new SqlConnection(source);
+			connection.Open();
+			SqlTransaction transaction = connection.BeginTransaction(IsolationLevel.ReadUncommitted);
+			try
+			{
+				SqlCommand cmd = new SqlCommand();
+				cmd.Transaction = transaction;
+				cmd.Connection = connection;
+				cmd.CommandText = "DELETE FROM Region WHERE RegionID = 6";
+				int rows = cmd.ExecuteNonQuery();
+				transaction.Commit();
+				LogHelper.Log($"删除成功条数：{rows}");
+			}
+			catch (Exception ex)
+			{
+				LogHelper.Error(ex.Message);
+				transaction.Rollback();
+			}
+			finally
+			{
+				connection.Close();
+			}
+		}
+
+		public void TestIsolationLevel4()
+		{
+			string source = "server=10.5.67.45;database=Northwind;uid=tomcat;pwd=LuoHao123;";
+			SqlConnection connection = new SqlConnection(source);
+			connection.Open();
+			SqlTransaction transaction = connection.BeginTransaction(IsolationLevel.ReadUncommitted);
+			try
+			{
+				SqlCommand cmd = new SqlCommand();
+				cmd.Transaction = transaction;
+				cmd.Connection = connection;
+				cmd.CommandText = "INSERT INTO Region(RegionID,RegionDescription) VALUES(6,'EastNorth')";
+				int rows = cmd.ExecuteNonQuery();
+				transaction.Commit();
+				LogHelper.Log($"新增成功条数：{rows}");
+			}
+			catch (Exception ex)
+			{
+				LogHelper.Error(ex.Message);
+				transaction.Rollback();
+			}
+			finally
+			{
+				connection.Close();
+			}
+		}
 	}
 }
