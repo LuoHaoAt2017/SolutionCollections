518e7a8ae5d0607f97af6b2c4ad4424686929971
LuoHao <hluo5206@gmail.com>
2022-12-21 12:03:36 +0800

    feat: 事件，使用事件传递参数

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,17 +1,21 @@
 ﻿
 using ConsoleApp.Models;
 
+// 通过委托类型来描述事件和事件处理程序
+
 public class Program
 {
 	public static void Main()
 	{
-		Film film;
-		int index;
+		//Film film = new Film();
+		//Console.WriteLine(film.GetType().ToString());
+		//Console.WriteLine(typeof(Film));
+		//int index;
 		//MethodOut1(out index);
 		//MethodRef1(ref index);
 		//MethodOut2(out film);
 		//MethodRef2(ref film);
-	}
+    }
 
 	public static Film Go()
 	{
@@ -48,4 +52,6 @@ public class Program
 		film.FilmGuid = DateTime.Now.ToString();
 		film.FilmName = "寻仇三部曲";
 	}
+
+
 }
